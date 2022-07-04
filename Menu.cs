ba60a5a6be623793dd30a7825e8515f3a1617bf4
stefanom13 <stefanomancuso13@gmail.com>
2022-07-04 17:44:41 +0200

    traccia

--- Menu.cs
+++ Menu.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿//Nel programma della banca si vuole gestire anche l'apertura di un conto che può essere di due tipi:
+
+//conto bancario classico:
+//l 'utente può prelevare e depositare denaro a piacere. ovviamente non può prelevare denaro se non ne ha
+
+//nel conto bancario risparmio:
+//l'utente può prelevare anche 50€ purchè ci siano almeno 1000 euro e può versare al massimo 5000 euro.
+//Ovviamente non può prelevare somme che non ha in deposito.
+
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
